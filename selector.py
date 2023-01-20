152c6473453c91b8f3b82e1c7b99b8f03c3aed13
xfeehrxx <f.rafaeladev@gmail.com>
2023-01-19 21:11:50 -0300

    remoção de código desnecessario

--- selector.py
+++ selector.py
@@ -7,20 +7,6 @@ hdr = {
     'X-MAL-CLIENT-ID' : config['clientId']
     }
 
-def animeValidation(a = {'id1', 'id2'}):
-        with req.get('https://api.myanimelist.net/v2/anime/' + str(a[0]['id1']) + '?fields=end_date,synopsis,status, related_anime', headers=hdr) as r:
-            dt1 = json.loads(r.text)['end_date']
-            dt1 = dt.strptime(dt1,'%Y-%m-%d').date()
-
-        with req.get('https://api.myanimelist.net/v2/anime/' + str(a[0]['id2']) + '?fields=end_date,synopsis,status, related_anime', headers=hdr) as r:
-            dt2 = json.loads(r.text)['end_date']
-            dt2 = dt.strptime(dt2,'%Y-%m-%d').date()
-
-        if (dt1 == min([dt1, dt2])):
-            return a[0]['id2']
-        else:
-             return a[0]['id1']
-
 animes = []
 with req.get('https://api.myanimelist.net/v2/users/' + config['user'] + '/animelist?status=plan_to_watch&limit=500', headers=hdr) as r:
     for node in json.loads(r.text)['data']:
@@ -28,17 +14,12 @@ with req.get('https://api.myanimelist.net/v2/users/' + config['user'] + '/animel
 
 x  = []
 for anime in animes:
-    for i in range(1, len(animes)):
+    for i in range(list(animes).index(anime) + 1, len(animes)):
         if(anime['title'].split(' ')[0] == animes[i]['title'].split(' ')[0]):
-            x.append({'id1': anime['id'], 'id2': animes[i]['id']})
-            v = animeValidation(x)  
-            if(anime['id'] == v):
-                animes.remove(anime)
-            else:
-                animes.remove(animes[i])
-        break
-
-    print('entrou')
+            animes.remove(animes[i])
+            i -=1
+        else:
+            break
       
     
 print('Encerrado.')
